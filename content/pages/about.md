6727af9fb464cb52de4bf12e340eeb0e9cf827ae
Timothy Crosley <timothy.crosley@gmail.com>
2018-03-12 00:35:17 -0700

    first commit

---
+++ content/pages/about.md
@@ -0,0 +1,11 @@
+Title: About
+Date: 2018-03-11 17:18
+Category: Information
+
+![Potect your community. Preserve Life.](/images/about.jpg)
+
+Welcome to our blog! We created LoveLicton as a community resource to cover news, retail openings, and events happening in the beautiful Licton Springs neighborhood of Seattle and surrounding areas. Licton Springs, located between I5 and Aurora Ave., is uniquely well connected to the rest of Seattle, while also having many amenities of its own. Living in Licton Springs means walkable access to the Licton Springs Park, the Oak Tree Plaza, the Green Lake Park, the Greenwood Strip, the Maple Leaf Strip, and the Northgate Mall. If you find yourself in the Licton Springs area, as a neighbor or just passing through, we hope you find some inspiration for how to enjoy your time in this blog!
+
+Thanks!
+
+The Crosleys
---
+++ pelicanconf.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'The Crosleys'
+SITENAME = 'Love Licton'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'US/Pacific'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+THEME = 'attila'
+HEADER_COVER = 'images/header.jpg'
+SITE_LOGO = '/images/logo.png'
+CSS_OVERRIDE = ['static/main.css']
+STATIC_PATHS = ['static',
+                'images',
+                'extra/robots.txt', 
+                'extra/favicon.ico'
+]
+EXTRA_PATH_METADATA = {
+    'extra/robots.txt': {'path': 'robots.txt'},
+    'extra/favicon.ico': {'path': 'favicon.ico'}
+}
+
+COLOR_SCHEME_CSS = 'monokai.css'
+AUTHORS_BIO = {
+  "zutrinken": {
+    "name": "Zutrinken",
+    "cover": "https://arulrajnet.github.io/attila-demo/assets/images/avatar.png",
+    "image": "https://arulrajnet.github.io/attila-demo/assets/images/avatar.png",
+    "website": "http://blog.arulraj.net",
+    "location": "Chennai",
+    "bio": "This is the place for a small biography with max 200 characters. Well, now 100 are left. Cool, hugh?"
+  }
+}
+SHOW_CREDITS = {'left': 'Made in Licton Springs, Seattle, WA',
+                'right': 'Composed by the Crosleys'}
+  
+SOCIAL = (('twitter', 'https://twitter.com/lictonlove'),
+          ('facebook','https://facebook.com/lovelicton'),
+          ('envelope','mailto:timothy.crosley@gmail.com'))
+
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+DEFAULT_PAGINATION = 10
+DISQUS_SITENAME = 'lovelicton'
+GOOGLE_ANALYTICS = 'UA-115518273-1'
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
